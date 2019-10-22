493897ff662aa2c04e9d6801eb37606426848ec7
emanueleghebaur <37125221+emanueleghebaur@users.noreply.github.com>
2019-10-23 00:46:49 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,17 @@
 var mic;
+var incr = 0;
+var pos = 0;
+
+function preload(){
+  // Create an Audio input
+  mic = new p5.AudioIn();
+  // start the Audio Input.
+}
 
 function setup() {
   createCanvas(500, 500);
-
-  // Create an Audio input
-  mic = new p5.AudioIn();
-
-  // start the Audio Input.
   mic.start();
+
 }
 
 function draw() {
@@ -15,6 +19,15 @@ function draw() {
 
   // Get the overall volume (between 0 and 1.0)
   var vol = mic.getLevel();
-  ellipse(250,250,vol * 200);
+  console.log(vol);
+  ellipse(250, 250, vol * 200);
 
+}
+
+function Blocco(_h) {
+  this.x = rand(50, 500);
+  this.y = height;
+  this.w = rand(50, 300);
+  this.h = -_h;
+  fill(0);
 }
